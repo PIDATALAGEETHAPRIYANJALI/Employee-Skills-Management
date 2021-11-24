d2a17d945b67d004364088d91d461ca99006aa2b
pgeetha <pgeetha@pgeetha-stratapps>
2021-11-24 11:42:38 +0530

    the second commit

---
+++ Employee-Skills-Management/src/main/java/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+	
+	 @Query("SELECT DISTINCT user FROM User user " +
+	            "INNER JOIN FETCH user.authorities AS authorities " +
+	            "WHERE user.username = :username")
+	    User findByUsername(@Param("username") String username);
+
+}

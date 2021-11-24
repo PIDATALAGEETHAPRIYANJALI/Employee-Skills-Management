5bec86b2c4b6db414a3585e4b02635459b981e91
pgeetha <pgeetha@pgeetha-stratapps>
2021-11-24 10:56:27 +0530

    the first commit

---
+++ Employee-Skills-Management/src/test/java/com/employeeskillmanagement/EmployeeSkillsManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employeeskillmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeSkillsManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
